1af7dcb35b8357ebef42c470c12d347123d51759
FedeCraviotto <fcraviotto.dev@gmail.com>
2023-02-17 20:03:16 -0300

    Add payment to general setting apps View for Braintree token

--- backend/payment/views.py
+++ backend/payment/views.py
@@ -1,5 +1,8 @@
 from django.conf import settings
 import braintree
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 #Con esto seteado podemos crear customers, addresses, paymentMethods
 gateway = braintree.BraintreeGateway(
@@ -10,3 +13,17 @@ gateway = braintree.BraintreeGateway(
         private_key=settings.BT_PRIVATE_KEY
     )
 )
+
+class GenerateTokenView(APIView):
+    def get(self, request, format=None):
+        try:
+            client_token = gateway.client_token.generate()
+            return Response(
+                {'token': client_token},
+                status=status.HTTP_200_OK
+            )
+        except:
+            return Response(
+                {'error': 'Something went wrong when retrieving Braintree token'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
--- backend/showtech/settings.py
+++ backend/showtech/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'payment',
 ]
 
 MIDDLEWARE = [
